d2b2d3eaa7fba0d7b3c557eccbb9a05f8478d909
decodersifat <user.sifathossain@gmail.com>
2025-07-18 03:55:19 +0600

    Added Routing and Email validation: Unique, xxx@xxx.xxx pattern validation

--- backend/express/server.js
+++ backend/express/server.js
@@ -4,9 +4,12 @@ const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 const User = require('./models/user.models');
+const ProfileRoutes = require('./routes/profile');
+
 
 app.use(cors());
 app.use(express.json());
+app.use('/api/v1/user', ProfileRoutes);
 
 mongoose.connect(process.env.MONGODB_CONN_STRING).then(() => {
     console.log("Connected To Database");
@@ -14,14 +17,7 @@ mongoose.connect(process.env.MONGODB_CONN_STRING).then(() => {
     console.log("Connection Failed");
 });
 
-app.post('/api/signup', async (req, res) => {
-    try {
-        const user = await User.create(req.body);
-        res.status(200).json(user);
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
-});
+
 
 app.post('/api/login', async (req, res) => {
     const { email, password } = req.body;
